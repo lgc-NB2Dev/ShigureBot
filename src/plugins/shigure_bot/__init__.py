3e4b21ef789c508a1633feb699d9b3a8dff13293
student_2333 <lgc2333@126.com>
2022-04-18 01:37:57 +0800

    fix

--- src/plugins/shigure_bot/__init__.py
+++ src/plugins/shigure_bot/__init__.py
@@ -1,10 +1,14 @@
+import sys
 from pathlib import Path
 
 import nonebot
 
-_sub_plugins = set()
-_sub_plugins |= nonebot.load_plugins(
-    str((Path(__file__).parent / "plugins").
-        resolve()))
+path = str((Path(__file__).parent / "plugins").
+           resolve())
 
-__version__ = '0.1.1'
+sys.path.append(path)
+
+_sub_plugins = set()
+_sub_plugins |= nonebot.load_plugins(path)
+
+__version__ = '0.1.2'
