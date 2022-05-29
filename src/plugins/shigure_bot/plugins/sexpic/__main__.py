a3229672a878b4237cab7b7a7a9eba49532712cf
student_2333 <lgc2333@126.com>
2022-05-29 12:34:55 +0800

    0.4.1 sexpic: add conf "can_filter_by_tag"

--- src/plugins/shigure_bot/plugins/sexpic/__main__.py
+++ src/plugins/shigure_bot/plugins/sexpic/__main__.py
@@ -3,7 +3,7 @@ import time
 
 from nonebot import on_message
 from nonebot.adapters.onebot.v11 import ActionFailed, Bot
-from nonebot.adapters.onebot.v11.event import MessageEvent
+from nonebot.adapters.onebot.v11.event import GroupMessageEvent, PrivateMessageEvent
 from nonebot.matcher import Matcher
 
 from .config import config as conf
@@ -15,7 +15,7 @@ temp = {'group': {}, 'private': {}}
 @on_message(
     rule=lambda event: str(event.get_message()).startswith(tuple(conf.trigger_words))
 ).handle()
-async def _(bot: Bot, event: MessageEvent, matcher: Matcher):
+async def _(bot: Bot, event: PrivateMessageEvent | GroupMessageEvent, matcher: Matcher):
     msg = str(event.get_message())
     if conf.only_private and event.message_type != 'private':
         return
@@ -32,7 +32,6 @@ async def _(bot: Bot, event: MessageEvent, matcher: Matcher):
 
         if time_passed >= conf.delay:
             tmp_dict[chat_id] = time.time()
-            await matcher.send('图片正在来的路上~\nPictures from Lolicon API')
 
             tag = ''
             for kw in conf.trigger_words:
@@ -40,20 +39,25 @@ async def _(bot: Bot, event: MessageEvent, matcher: Matcher):
                 if tag != msg:
                     break
 
-            try:
-                ret, clear = await get_pic(event.sender.user_id, tag.strip())
-                msg_id = (await matcher.send(ret))['message_id']
-            except ActionFailed:
-                clear = True
-                await matcher.finish('消息发送失败…', at_sender=True)
+            clear = True
+            tag = tag.strip()
+            if (not conf.can_filter_by_tag) and tag:
+                await matcher.send('已关闭tag筛选功能，请不要使用tag筛选')
+            else:
+                await matcher.send('图片正在来的路上~\nPictures from Lolicon API')
+                try:
+                    ret, clear = await get_pic(event.sender.user_id, tag)
+                    msg_id = (await matcher.send(ret))['message_id']
+                except ActionFailed:
+                    await matcher.finish('消息发送失败…', at_sender=True)
 
-            if conf.withdraw_delay and (not clear):
-                # https://github.com/nonebot/adapter-onebot/commit/06956d210d70d4fc2471f39a97ec45f1995f5f0d
-                loop = asyncio.get_running_loop()
-                return loop.call_later(
-                    conf.withdraw_delay,
-                    lambda: loop.create_task(bot.delete_msg(message_id=msg_id)),
-                )
+                if conf.withdraw_delay and (not clear):
+                    # https://github.com/nonebot/adapter-onebot/commit/06956d210d70d4fc2471f39a97ec45f1995f5f0d
+                    loop = asyncio.get_running_loop()
+                    return loop.call_later(
+                        conf.withdraw_delay,
+                        lambda: loop.create_task(bot.delete_msg(message_id=msg_id)),
+                    )
 
             if clear:
                 tmp_dict[chat_id] = None
@@ -61,4 +65,4 @@ async def _(bot: Bot, event: MessageEvent, matcher: Matcher):
             await matcher.finish(f'图片冷却中……请等{conf.delay - time_passed}秒再来吧', at_sender=True)
 
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
--- src/plugins/shigure_bot/plugins/sexpic/config.py
+++ src/plugins/shigure_bot/plugins/sexpic/config.py
@@ -14,6 +14,7 @@ class ConfigModel(BaseModel):
     proxy: str
     withdraw_delay: int
     send_details: bool
+    can_filter_by_tag: bool
 
 
 async def update_conf():
@@ -21,12 +22,13 @@ async def update_conf():
     config = await init(
         'sexpic',
         ConfigModel,
-        {'only_private'  : False,
-         'trigger_words' : ['色图', '涩图', '瑟图', '二次元', '二刺猿', '二刺螈'],
-         'delay'         : 120,
-         'proxy'         : 'http://127.0.0.1:10809',
-         'withdraw_delay': 0,
-         'send_details'  : True}
+        {'only_private'     : False,
+         'trigger_words'    : ['色图', '涩图', '瑟图', '二次元', '二刺猿', '二刺螈'],
+         'delay'            : 120,
+         'proxy'            : 'http://127.0.0.1:10809',
+         'withdraw_delay'   : 0,
+         'send_details'     : True,
+         'can_filter_by_tag': True}
     )
 
 
