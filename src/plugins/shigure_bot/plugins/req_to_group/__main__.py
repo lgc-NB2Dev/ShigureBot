a5d481299715304967a4281c04bb9487484ced0a
lgc2333 <git>
2022-08-07 09:31:29 +0000

    Use Black to Format Code

--- src/plugins/shigure_bot/plugins/req_to_group/__main__.py
+++ src/plugins/shigure_bot/plugins/req_to_group/__main__.py
@@ -2,8 +2,14 @@ import random
 import string
 
 from nonebot import logger, on_command, on_request
-from nonebot.adapters.onebot.v11 import (ActionFailed, Bot, GroupMessageEvent, GroupRequestEvent,
-                                         Message, MessageSegment)
+from nonebot.adapters.onebot.v11 import (
+    ActionFailed,
+    Bot,
+    GroupMessageEvent,
+    GroupRequestEvent,
+    Message,
+    MessageSegment,
+)
 from nonebot.matcher import Matcher
 from nonebot.params import CommandArg
 
@@ -12,25 +18,25 @@ tmp = {}
 
 
 def get_random_str(length: int = 6):
-    return ''.join(random.sample(f'{string.ascii_letters}{string.digits}', length))
+    return "".join(random.sample(f"{string.ascii_letters}{string.digits}", length))
 
 
 async def get_stranger_name(bot: Bot, qq):
     try:
-        return (await bot.get_stranger_info(user_id=qq))['nickname']
+        return (await bot.get_stranger_info(user_id=qq))["nickname"]
     except ActionFailed:
-        return '未知'
+        return "未知"
 
 
 async def get_group_name(bot: Bot, group_id):
     try:
-        return (await bot.get_group_info(group_id=group_id))['group_name']
+        return (await bot.get_group_info(group_id=group_id))["group_name"]
     except ActionFailed:
-        return '未知'
+        return "未知"
 
 
 def group_invite_rule(event: GroupRequestEvent):
-    return event.sub_type == 'invite'
+    return event.sub_type == "invite"
 
 
 @on_request(rule=group_invite_rule).handle()
@@ -40,10 +46,12 @@ async def _(bot: Bot, event: GroupRequestEvent):
     tmp[rdm := get_random_str()] = event
     await bot.send_group_msg(
         group_id=fwd_group_id,
-        message=MessageSegment.at(event.user_id) +
-                (f'\n'
-                 f'{username}({event.user_id}) 邀请我进群 {group_name}({event.group_id})\n'
-                 f'请您在本群发送 同意请求{rdm} 以自动同意该邀请进群请求')
+        message=MessageSegment.at(event.user_id)
+        + (
+            f"\n"
+            f"{username}({event.user_id}) 邀请我进群 {group_name}({event.group_id})\n"
+            f"请您在本群发送 同意请求{rdm} 以自动同意该邀请进群请求"
+        ),
     )
 
 
@@ -51,8 +59,10 @@ def group_rule(event: GroupMessageEvent):
     return event.group_id == fwd_group_id
 
 
-@on_command('同意请求', rule=group_rule).handle()
-async def _(bot: Bot, event: GroupMessageEvent, matcher: Matcher, args: Message = CommandArg()):
+@on_command("同意请求", rule=group_rule).handle()
+async def _(
+    bot: Bot, event: GroupMessageEvent, matcher: Matcher, args: Message = CommandArg()
+):
     rdm = args.extract_plain_text().strip()
     req: GroupRequestEvent = tmp.get(rdm)
     if req:
@@ -60,13 +70,13 @@ async def _(bot: Bot, event: GroupMessageEvent, matcher: Matcher, args: Message 
             try:
                 await req.approve(bot)
             except Exception as e:
-                logger.opt(exception=e).exception('同意请求失败')
+                logger.opt(exception=e).exception("同意请求失败")
                 await matcher.finish(f'同意请求失败，请重试\n{"；".join(e.args)}')
             else:
-                await matcher.finish('同意请求成功！')
+                await matcher.finish("同意请求成功！")
 
             del tmp[rdm]
         else:
-            await matcher.finish('该请求不是你发起的！')
+            await matcher.finish("该请求不是你发起的！")
 
-    await matcher.send('未找到请求')
+    await matcher.send("未找到请求")
