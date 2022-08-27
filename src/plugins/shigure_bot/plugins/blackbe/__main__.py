687c50f3ba2ba0ea435b0078c601e00176351c35
student_2333 <lgc2333@126.com>
2022-08-27 23:53:50 +0800

    将云黑检测的缓存期限提高到2小时 加入`清除云黑缓存`指令用于手动清除缓存

--- src/plugins/shigure_bot/plugins/blackbe/__main__.py
+++ src/plugins/shigure_bot/plugins/blackbe/__main__.py
@@ -11,6 +11,16 @@ detect = BlackBEDetect()
 scheduler = require("nonebot_plugin_apscheduler").scheduler
 
 
+@scheduler.scheduled_job("cron", minute="*/10")
+async def _schedule_clear_noticed():
+    detect.tmp_noticed.clear()
+
+
+@scheduler.scheduled_job("cron", hour="*/12")
+async def _schedule_clear_tmp():
+    detect.tmp_black.clear()
+
+
 def list_has_same_item(list1, list2):
     for i in list1:
         if i in list2:
@@ -18,22 +28,24 @@ def list_has_same_item(list1, list2):
     return False
 
 
-@scheduler.scheduled_job("cron", minute="*/10")
-async def delete_temp():
-    detect.clear_tmp()
-
-
 @on_command("查云黑").handle()
 async def _(
-    bot: Bot, event: MessageEvent, matcher: Matcher, args: Message = CommandArg()
+        bot: Bot, event: MessageEvent, matcher: Matcher, args: Message = CommandArg()
 ):
     if at := args["at"]:
         await send_info_msg(bot, event, qq=at[0].data["qq"])
     else:
         if not (msg := args.extract_plain_text().strip()):
-            await matcher.finish(" \n指令格式：查云黑<XboxID/QQ号/@某人/XUID>", at_sender=True)
+            await matcher.finish("\n指令格式：查云黑<XboxID/QQ号/@某人/XUID>", at_sender=True)
         else:
             await send_info_msg(bot, event, name=msg, qq=msg, xuid=msg)
+
+
+@on_command('清除云黑缓存').handle()
+async def _(matcher: Matcher):
+    detect.tmp_noticed.clear()
+    detect.tmp_black.clear()
+    await matcher.send('清除成功')
 
 
 @on_message(block=False).handle()
@@ -43,7 +55,7 @@ async def _(event: GroupMessageEvent, matcher: Matcher):
         if ret and ret.data:
             if ret.data.exist:
                 if not list_has_same_item(
-                    conf.ignore_repos, [x.uuid for x in ret.data.info]
+                        conf.ignore_repos, [x.uuid for x in ret.data.info]
                 ):
                     await matcher.send(
                         f"在BlackBE存在违规记录！\n" f"使用 查云黑{event.user_id} 查询详细信息",
--- src/plugins/shigure_bot/plugins/blackbe/detect.py
+++ src/plugins/shigure_bot/plugins/blackbe/detect.py
@@ -1,12 +1,9 @@
-import copy
-
 from nonebot import logger
 
 from .config import config as conf
 from .datatypes import BlackBEReturn
 from .get_data import get_simple_info
 
-_tmp_template: dict = {"black": {}, "noticed": {}}
 _ret_template = BlackBEReturn(
     **{
         "success": True,
@@ -21,32 +18,30 @@ _ret_template = BlackBEReturn(
 
 
 class BlackBEDetect:
-    tmp: dict
+    tmp_black: dict
+    tmp_noticed: dict
 
     def __init__(self):
-        self.clear_tmp()
-
-    def clear_tmp(self):
-        self.tmp = copy.deepcopy(_tmp_template)
+        pass
 
     def set_notice_object(self, group):
-        if not self.tmp["noticed"].get(group):
-            self.tmp["noticed"][group] = {}
+        if not self.tmp_noticed.get(group):
+            self.tmp_noticed[group] = {}
 
     def has_noticed(self, group, qq):
         self.set_notice_object(group)
-        return self.tmp["noticed"][group].get(qq) == True
+        return self.tmp_noticed[group].get(qq) == True
 
     def set_notice_stat(self, group, qq, stat):
         self.set_notice_object(group)
-        self.tmp["noticed"][group][qq] = stat
+        self.tmp_noticed[group][qq] = stat
 
     async def detect(self, qq):
-        if (ret := self.tmp["black"].get(qq)) is None:
-            self.tmp["black"][qq] = _ret_template  # 避免同时间多次访问
+        if (ret := self.tmp_black.get(qq)) is None:
+            self.tmp_black[qq] = _ret_template  # 避免同时间多次访问
             ret = await get_simple_info(token=conf.token, qq=qq)
             if isinstance(ret, Exception):
                 logger.opt(exception=ret).exception("查询云黑记录失败")
                 ret = None
-            self.tmp["black"][qq] = ret
+            self.tmp_black[qq] = ret
         return ret
