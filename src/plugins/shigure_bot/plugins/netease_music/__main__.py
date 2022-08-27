4bdc9ea0cdedd385a2bafcabfd50de2987f849f3
lgc2333 <lgc2333@126.com>
2022-08-27 18:27:04 +0000

    Use Black to Format Code

--- src/plugins/shigure_bot/plugins/netease_music/__main__.py
+++ src/plugins/shigure_bot/plugins/netease_music/__main__.py
@@ -8,7 +8,7 @@ from nonebot.typing import T_State
 
 from .data_source import get_track_audio, login, search
 
-pick_matcher = on_command('点歌', aliases={'网易云点歌', '网易点歌', 'wyy', 'netease', 'ncm'})
+pick_matcher = on_command("点歌", aliases={"网易云点歌", "网易点歌", "wyy", "netease", "ncm"})
 
 
 def format_song_name(song):
@@ -35,7 +35,7 @@ async def _(m: Matcher, state: T_State, name: str = ArgPlainText("song")):
     try:
         ret = await search(name)
     except:
-        logger.exception('网易云搜索失败')
+        logger.exception("网易云搜索失败")
         return await m.finish("搜索歌曲失败，请检查后台输出")
 
     if ret["code"] != 200:
@@ -45,36 +45,33 @@ async def _(m: Matcher, state: T_State, name: str = ArgPlainText("song")):
     if not ret.get("songs"):
         return await m.finish("未搜索到歌曲")
 
-    state['ret'] = ret
+    state["ret"] = ret
 
     tmp = []
     for i, song in enumerate(ret["songs"]):
-        tmp.append(f'{i + 1}. {format_song_name(song)} - {format_song_ars(song)}')
+        tmp.append(f"{i + 1}. {format_song_name(song)} - {format_song_ars(song)}")
 
-    song_li = '\n'.join(tmp)
-    await m.send(
-        f'【{name}】的搜索结果：\n\n{song_li}\n\n'
-        f'Tip：直接发送歌曲序号即可选择'
-    )
+    song_li = "\n".join(tmp)
+    await m.send(f"【{name}】的搜索结果：\n\n{song_li}\n\n" f"Tip：直接发送歌曲序号即可选择")
 
 
 @pick_matcher.got("index")
-async def _(m: Matcher, ret: dict = Arg('ret'), index: str = ArgPlainText("index")):
+async def _(m: Matcher, ret: dict = Arg("ret"), index: str = ArgPlainText("index")):
     index = index.strip()
     if not index.isdigit():
         await m.reject("选项只能为整数，请重新选择")
 
     index = int(index)
-    list_len = len(ret['songs'])
+    list_len = len(ret["songs"])
     if not 1 <= index <= list_len:
         await m.reject(f"选项只能为1~{list_len}，请重选")
 
-    song = ret['songs'][index - 1]
-    song_id = song['id']
+    song = ret["songs"][index - 1]
+    song_id = song["id"]
     try:
-        ret_down = (await get_track_audio(
-            [song_id], bit_rate=song["privilege"]["playMaxbr"]
-        ))["data"]
+        ret_down = (
+            await get_track_audio([song_id], bit_rate=song["privilege"]["playMaxbr"])
+        )["data"]
     except:
         logger.exception("获取歌曲播放链接失败")
         return await m.finish("获取歌曲播放链接失败，请重试")
@@ -87,19 +84,19 @@ async def _(m: Matcher, ret: dict = Arg('ret'), index: str = ArgPlainText("index
     title = format_song_name(song)
     ars = format_song_ars(song)
     await m.send(
-        f'为您送上 {ars} 的歌曲 {title} ~\n'
+        f"为您送上 {ars} 的歌曲 {title} ~\n"
         f'该歌曲音质为 {ret_down["level"]}，\n'
         f'歌曲文件大小为 {round(ret_down["size"] / 1024 / 1024, 2)} MB，\n'
-        f'请注意流量消耗'
+        f"请注意流量消耗"
     )
     await m.send(
         MessageSegment.music_custom(
-            audio_url, audio_url, title, ars, song['al']['picUrl']
+            audio_url, audio_url, title, ars, song["al"]["picUrl"]
         )
     )
 
 
-@on_command('网易云重登', permission=SUPERUSER).handle()
+@on_command("网易云重登", permission=SUPERUSER).handle()
 async def _(m: Matcher):
     ret = await login()
     if isinstance(ret, Exception):
