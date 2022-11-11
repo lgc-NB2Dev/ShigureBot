9ec349feec609d27ca8a82dded9b4cb6bb17b51c
student_2333 <lgc2333@126.com>
2022-11-11 20:36:36 +0800

    fix mcstatus

--- src/plugins/shigure_bot/plugins/mc_svr_stat/get_stat.py
+++ src/plugins/shigure_bot/plugins/mc_svr_stat/get_stat.py
@@ -1,5 +1,5 @@
-import mcstatus as mc
 import nonebot
+from mcstatus import BedrockServer, JavaServer
 from nonebot.adapters.onebot.v11 import MessageSegment
 
 
@@ -15,7 +15,7 @@ def del_escape(string: str):
 
 
 async def motd(address: str, full=False):
-    address = address.strip()
+    address = address.strip().replace("：",":")
     if not address:
         if full:
             return MessageSegment.text(
@@ -35,26 +35,26 @@ async def motd(address: str, full=False):
         address += ":25565"
 
     try:
-        ret = mc.MinecraftServer.lookup(address)
+        ret = JavaServer.lookup(address)
         ret = await ret.async_status()
         latency = ret.latency
         desc = ret.description
-        ret = ret.raw
+        ret_raw = ret.raw
     except Exception as e:
         nonebot.logger.opt(colors=True, exception=e).error("")
         return MessageSegment.text(f"[MCJE服务器信息]\n操作失败：{e!r}")
 
     pic_text = ""
-    if "favicon" in ret:
-        pic_b64: str = ret["favicon"]
+    if ret.favicon:
+        pic_b64: str = ret.favicon
         pos = pic_b64.find("base64,")
         if pos != -1:
-            pic_b64 = pic_b64[pos + 7 :]
+            pic_b64 = pic_b64[pos + 7:]
         pic_text = "显示图标：" + MessageSegment.image(f"base64://{pic_b64}") + "\n"
 
     players = ""
-    if "sample" in ret["players"]:
-        player_list = [del_escape(x["name"]) for x in ret["players"]["sample"]]
+    if ret.players.sample:
+        player_list = [del_escape(x.name) for x in ret.players.sample]
         player_list = [x for x in player_list if x]
         if full:
             players = f'玩家列表：{", ".join(player_list)}\n'
@@ -62,11 +62,11 @@ async def motd(address: str, full=False):
             players = f'玩家列表（最多显示十个）：{", ".join(player_list[:10])}\n'
 
     mod_info = ""
-    if "modinfo" in ret:
-        if ret["modinfo"]["modList"]:
+    if "modinfo" in ret_raw:
+        if ret_raw["modinfo"]["modList"]:
             mod_list = "\n".join(
                 [f'{x["modid"]} - {x["version"]}' for x in ret["modinfo"]["modList"]][
-                    :10
+                :10
                 ]
             )
             if full:
@@ -77,30 +77,30 @@ async def motd(address: str, full=False):
             mod_info = "Mod列表：无"
         mod_info = f'\nMod端类型：{ret["modinfo"]["type"]}\n{mod_info}'
 
-    players_online = ret["players"]["online"]
-    players_max = ret["players"]["max"]
+    players_online = ret.players.online
+    players_max = ret.players.max
     online_percent = round(players_online / players_max * 100, 2)
 
     full_tip = "" if full else f"\n注：使用 !motdfull 查看全部玩家列表及mod列表"
 
     return (
-        MessageSegment.text(f"[MCJE服务器信息]\n")
-        + pic_text
-        + MessageSegment.text(
-            f'服务端名：{ret["version"]["name"]}\n'
-            f'协议版本：{ret["version"]["protocol"]}\n'
-            f"当前人数：{players_online}/{players_max}({online_percent}%)\n"
-            f"{players}"
-            f"描述文本：\n{del_escape(desc)}\n"
-            f"游戏延迟：{latency}ms"
-            f"{mod_info}"
-            f"{full_tip}"
-        )
+            MessageSegment.text(f"[MCJE服务器信息]\n")
+            + pic_text
+            + MessageSegment.text(
+        f'服务端名：{ret.version.name}\n'
+        f'协议版本：{ret.version.protocol}\n'
+        f"当前人数：{players_online}/{players_max}({online_percent}%)\n"
+        f"{players}"
+        f"描述文本：\n{del_escape(desc)}\n"
+        f"游戏延迟：{latency:.2f}ms"
+        f"{mod_info}"
+        f"{full_tip}"
+    )
     )
 
 
 async def motdpe(address: str):
-    address = address.strip()
+    address = address.strip().replace("：",":")
     if not address:
         return MessageSegment.text(
             "[MCBE服务器信息]\n" "正确用法:\n" "!motd <服务器IP> (电脑版)\n" "!motdpe <服务器IP> (手机版)"
@@ -109,7 +109,7 @@ async def motdpe(address: str):
         address += ":19132"
 
     try:
-        ret = mc.MinecraftBedrockServer.lookup(address)
+        ret = BedrockServer.lookup(address)
         ret = await ret.async_status()
     except Exception as e:
         nonebot.logger.opt(colors=True, exception=e).error("")
@@ -127,5 +127,5 @@ async def motdpe(address: str):
         f"在线人数：{ret.players_online}/{ret.players_max}({online_percent}%)\n"
         f"{map_name}"
         f"{game_mode}"
-        f"游戏延迟：{round(ret.latency * 1000, 3)}ms"
+        f"游戏延迟：{ret.latency * 1000:.2f}ms"
     )
